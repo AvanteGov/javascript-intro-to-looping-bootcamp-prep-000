f81235678378b7af685f22c0530830d91e0da4a1
grateful-library-0184 <alexcouts18@gmail.com>
2019-03-02 22:02:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,8 @@ function forLoop(array){
   for (let i = 0; i < 25; i++){
    (i = 0){
       array.push("I am 1 strange loop.")
-     } else if (i > 1){
+     } 
+     else (i > 1){
         array.push("I am ${i} strange loop.")
       }
   }
